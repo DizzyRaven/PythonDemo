b8f56bbcf28990ad66d76afadb6bce2882e2a77d
DizzyRaven <akaynt47@gmail.com>
2019-01-13 16:18:48 +0200

    Test

--- Test_Python.py
+++ Test_Python.py
@@ -6,5 +6,4 @@ page = "https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0
 response = requests.get(page)
 soup = BeautifulSoup(response.text, 'html.parser')
 weather = soup.find	(id="today-temp")
-#weather = soup.select(".today-temp")
 print(weather[0].text)
