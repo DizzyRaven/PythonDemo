887938d3f9fd40d88fef7f140cbb0190db897e81
stevepoker <stevepokersx@gmail.com>
2019-01-16 14:56:41 +0200

    update

--- Test_Python.py
+++ Test_Python.py
@@ -5,10 +5,8 @@ from Human import Human
 page = "https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2"
 
 response = requests.get(page)
-print(response.text)
 soup = BeautifulSoup(response.text, 'html.parser')
 weather = soup.select(".today-temp")
-print(weather[0])
 print(weather[0].text)
 human = Human(23)
 print(human.age)
