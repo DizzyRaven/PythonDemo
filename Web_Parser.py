5f126e9ff6ab0bd2ca740c31d0d3faac4afe5e35
MrCrossGK <crosspocker@gmail.com>
2019-01-18 18:18:28 +0200

    Test

--- Web_Parser.py
+++ Web_Parser.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-from Human import Human
 
 page = "https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B8%D0%B5%D0%B2"
 
+class WebParser(object): #Зачем непосредственно необходим "object"
+    def 
 response = requests.get(page)
 soup = BeautifulSoup(response.text, 'html.parser')
 weather = soup.select(".today-temp")
 print(weather[0].text)
-human = Human(23)
-print(human.age)
-#Мдя а у меня не работают в этой среде все библиоткеки.. MrCross
