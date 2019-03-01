45bba413fbd19d128f1e0ce038470a420e113d84
MrCrossGK <mr.cross@ukr.net>
2019-03-01 20:12:39 +0200

    Update Lesson #11 И снова черепашья графика.py

--- Lessons/Lesson #11 И снова черепашья графика.py
+++ Lessons/Lesson #11 И снова черепашья графика.py
@@ -1,5 +1,114 @@
 #Начнем с обычного квадрата
+#import turtle
+#t = turtle.Pen()
+"""t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.reset()"""
+#==============================
+"""for x in range(0, 4):
+    t.forward(50)
+    t.left(90)"""
+#=====================================================================
+#Рисуем звезды
+""""for x in range(1, 9):
+    t.forward(100)
+    t.left(225)"""
+#==============================
+"""for x in range(1, 38):
+    t.forward(100)
+    t.left(175)"""
+#==============================
+"""for x in range(0, 190):
+    t.forward(100)
+    t.left(95)"""
+#==============================
+"""for i in range(0, 25):
+    t.left(90)
+    t.forward(20)
+    t.right(90)
+    t.forward(20)
+    t.right(90)
+    t.forward(20)
+    t.left(110)
+    t.forward(40)"""
+#==============================
+"""for x in range(0, 18):
+    t.forward(100)
+    if x % 2 == 0:
+        t.left(175)
+    else:
+        t.left(225)"""
+#==============================
 import turtle
 t = turtle.Pen()
-#=====================================================================
+"""t.reset()
+t.color(1,0,0)
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.right(90)
+t.forward(20)
+t.left(90)
+t.forward(60)
+t.left(90)
+t.forward(20)
+t.right(90)
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.end_fill()
+
+t.color(0,0,0)
+t.up()
+t.forward(10)
+t.down()
+t.begin_fill()
+t.circle(10)
+t.end_fill()
+
+t.setheading(0)
+t.up()
+t.forward(90)
+t.right(90)
+t.forward(10)
+t.setheading(0)
+t.begin_fill()
+t.down()
+t.circle(10)
+t.end_fill()"""
 #==============================
+#Возьмемся за краски
+"""t.color(0, 1, 1)
+t.begin_fill()
+t.circle(50)
+t.end_fill()"""
+#=====================================================================
+#Функция для рисования заполненной окружности
+"""def mycircle(red, green, blue, size):
+    t.color(red, green, blue)
+    t.begin_fill()
+    t.circle(size)
+    t.end_fill()"""
+#mycircle(0, 1, 0.5, 40)
+#mycircle(0.9, 0.75, 0, 50)
+#mycircle(1, 0.7, 0.75, 50)
+#=====================================================================
+#Получение черного и белого цвета
+#mycircle(0, 0, 0, 50)#Black
+#mycircle(1, 1, 1, 50)#White
+#=====================================================================
+#Функция рисования квадрата
+
+
+
+
+
+
